741dbc826527f5a4ccfadcc2156372e09cea8f5b
mimvoid <mimvoid@proton.me>
2024-12-04 00:01:08 -0500

    add nix dev environment

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake for dooit_scripts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    dooit.url = "github:dooit-org/dooit";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      allSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+
+      forAllSystems = f: allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "dooit_scripts";
+
+          packages =
+            let
+              python = pkgs.python312.withPackages (
+                ps: with ps; [
+                  click
+                ]
+              );
+            in
+            [
+              python
+              inputs.dooit.packages.${pkgs.system}.default
+            ];
+        };
+      });
+    };
+}
