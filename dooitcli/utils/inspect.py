e79b3084815c2ad789520f07c3504148d48fbf3b
mimvoid <mimvoid@proton.me>
2025-09-14 23:30:51 -0400

    Refactor OptionInspector

--- dooitcli/utils/inspect.py
+++ dooitcli/utils/inspect.py
@@ -3,21 +3,19 @@ Inspect types and the attributes and properties of
 Todo and Workspace objects for querying
 """
 
-from inspect import getmembers, ismethod, get_annotations, signature, Signature
-from typing import Any, ForwardRef
+from inspect import getmembers, get_annotations, signature, Signature
+from typing import ForwardRef, get_args
 
 from dooit.api import Todo, Workspace
+from sqlalchemy.orm.util import GenericAlias
 
 
-def return_sig_type(value: property) -> type:
-    return signature(value.fget).return_annotation
-
-
-def recurse_type(value_type: type) -> type:
-    if hasattr(value_type, "__args__"):
-        return recurse_type(value_type.__args__[0])
-
-    return value_type
+def has_forward_ref(value_type: type) -> bool:
+    args = get_args(value_type)
+    for arg in args:
+        if isinstance(arg, ForwardRef):
+            return True
+    return False
 
 
 def to_bool(value) -> bool:
@@ -34,54 +32,53 @@ def to_bool(value) -> bool:
 
 
 class OptionInspector:
-    attr: dict[str, Any] = dict()
-    prop: dict[str, property] = dict()
+    attr: dict[str, type] = dict()
+    prop: dict[str, type] = dict()
 
     # Only stores values can work as user inputs
-    input_attr: dict[str, Any] = dict()
-    input_prop: dict[str, property] = dict()
+    input_attr: dict[str, type] = dict()
+    input_prop: dict[str, type] = dict()
 
     def __init__(self, query_class):
         annotations = get_annotations(query_class)
 
         # Store public properties & attributes
         for name, value in getmembers(query_class):
-            if ismethod(value) or callable(value) or name.startswith("_"):
+            if callable(value) or name.startswith("_"):
                 continue
 
             if isinstance(value, property):
-                self._add_property(name, value)
+                return_type = signature(value.fget).return_annotation
+                self.prop[name] = return_type
+
+                # TODO: allow list query inputs
+                if return_type != Signature.empty and not has_forward_ref(return_type):
+                    self.input_prop[name] = return_type
             elif name in annotations:
-                self.attr[name] = annotations[name]
-                if not isinstance(recurse_type(value), ForwardRef):
-                    self.input_attr[name] = annotations[name]
+                attr_type = annotations[name]
+                if isinstance(attr_type, GenericAlias):
+                    attr_type = get_args(attr_type)[0]
 
-    def _add_property(self, name: str, value: property) -> None:
-        if name in ("has_same_parent_kind", "session"):
-            return
-
-        self.prop[name] = value
-
-        try:
-            # TODO: allow list query inputs
-            if value != Signature.empty and not issubclass(
-                return_sig_type(value), list
-            ):
-                self.input_prop[name] = value
-        except TypeError:
-            return
+                self.attr[name] = attr_type
+                if not has_forward_ref(attr_type):
+                    self.input_attr[name] = attr_type
 
     def get_type(self, name: str) -> type:
         if name in self.attr:
-            return recurse_type(self.attr[name])
+            return self.attr[name]
 
         if name in self.prop:
-            return return_sig_type(self.prop[name])
+            return self.prop[name]
 
         raise AttributeError("invalid attribute or property name: '%s'" % name)
 
     def get_type_str(self, name: str) -> str:
-        return self.get_type(name).__name__
+        type_obj = self.get_type(name)
+
+        if len(get_args(type_obj)) == 0:
+            return type_obj.__name__
+
+        return repr(type_obj).removeprefix(type_obj.__module__ + ".")
 
 
 todo_opts = OptionInspector(Todo)
